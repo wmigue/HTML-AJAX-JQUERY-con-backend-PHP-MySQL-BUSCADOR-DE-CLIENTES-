af00384bca5f8706d96690201a74d951e9cf8b80
unknown <wmigue@gmail.com>
2021-07-29 16:08:11 -0300

    2

--- listar_clientes.js
+++ listar_clientes.js
@@ -1,71 +1,46 @@
 
 
-		$(document).ready(function(){
+$(document).ready(function () {
 
-      var bus = document.getElementById("buscador"); 
+      var bus = document.getElementById("buscador");
       bus.focus();
       var html = "";
 
 
-		    $("#buscador").keyup(function(){
+      $("#buscador").keyup(function () {
+            $("#datos").html("");
+            let texto = $("#buscador").val();
+            console.log(texto);
 
-		    $("#datos").html("");
+            $.ajax({
+                  type: "POST",
+                  dataType: "JSON",
+                  data: ({ texto_buscado: texto }),
+                  url: "backend_listar_clientes.php",
 
-        
+                  success: function (data) {
+                        $.when(data).done(function () {
+                              $.each(data.mi_array, function (k, v) {
 
-                let texto = $("#buscador").val();
-                console.log(texto);
+                                    html += "<br>&nbsp; ";
+                                    html += '<div class="col-12 bg-light Cadena"><b>' + k + '</b>';
 
-               
+                                    $.each(v, function (nombre_atributo, atributo) {
 
-                $.ajax({
-                type: "POST",
-                dataType: "JSON",
-                data: ({ texto_buscado: texto}),
-                url: "backend_listar_clientes.php",
-                
-                success: function(data){
+                                          html += "<br>";
+                                          html += atributo;
 
-                   $.when(data).done(function(){
+                                    });
 
-
-                       	 $.each(data.mi_array, function(k,v){
-
-
-                           html += "<br>&nbsp; ";
-                           html += '<div class="col-12 bg-light Cadena"><b>'+k+'</b>';
-                           
-
-                                $.each(v, function(nombre_atributo, atributo){
-
-                                     
-                                      html += "<br>";
-                                      html += atributo;
-
-                                  
- 
-                       	 	 
-                   
-                                });
-
-                                     html += "</div>";
-                                     $("#datos").append(html);
-                                     html = "";
-                         
-                         });
-
-                  });
-                
-                        
-
-                }
-
-
-		         });
-
-	         });
-        
-        });
+                                    html += "</div>";
+                                    $("#datos").append(html);
+                                    html = "";
+                              });
+                        });
+                  }
+            });
+      });
+});
 
 
 
